5c5c9e11be71a1c52fae3fd8bcc49d081f2ddf17
Ayoub Gutin <gutinayoub@gmail.com>
2025-06-08 12:29:07 +0100

    started dev on calendar view

--- task_tracker/api.py
+++ task_tracker/api.py
@@ -2,12 +2,13 @@ from flask import Flask, render_template
 
 app = Flask(__name__, template_folder='../templates', static_folder='../static')
 
+
 # Main route
 @app.route('/')
 def index():
-    '''
+    """
     Render the main page
-    '''
+    """
     return render_template('index.html')
 
 
@@ -15,10 +16,14 @@ def index():
 def add_new_task_page():
     return render_template('add_task.html')
 
+
+@app.route('/calendar', methods=['GET'])
+def calendar_page():
+    return render_template('calendar.html')
+
+
 if __name__ == '__main__':
-    '''
+    """
     Run the Flask app
-    '''
+    """
     app.run(debug=True, port=5500)
-
-
